b627c8610d342c1a648f6197909d166a1f93cc8f
VARUN-688 <110732585+VARUN-688@users.noreply.github.com>
2024-05-27 21:41:00 +0530

    Update sih_results_scraper.py

--- sih_results_scraper.py
+++ sih_results_scraper.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-# Define URLs to scrape
 urls = [
     "https://www.sih.gov.in/sih2023-screening-result-batch1",
     "https://www.sih.gov.in/sih2023-screening-result-batch2",
@@ -13,34 +12,29 @@ urls = [
     "https://www.sih.gov.in/sih2023-screening-result-batch5"
 ]
 
-# Create a webdriver instance
 driver = webdriver.Chrome()
 
-# Iterate over each URL
 for idx, url in enumerate(urls, start=1):
     print(f"Scraping data from {url}")
     
-    # Navigate to the URL
     driver.get(url)
     driver.maximize_window()
-    time.sleep(5)  # Add a short delay to allow the page to load
+    time.sleep(5) 
     
-    # Find the table element
     table = driver.find_element(By.XPATH, '//*[@id="sheet0"]')
 
-    # Get the HTML content of the table
     table_html = table.get_attribute('outerHTML')
 
-    # Convert the HTML table to a Pandas DataFrame
+    
     df = pd.read_html(table_html)[0]
 
-    # Save the DataFrame to an Excel file
+    
     filename = f"batch-{idx}.xlsx"
     df.to_excel(filename, index=False)
     
     print(f"Table from {url} saved as {filename}")
 
-# Close the browser window
+
 driver.quit()
 
 print('All tables copied to Excel successfully using Selenium.')
